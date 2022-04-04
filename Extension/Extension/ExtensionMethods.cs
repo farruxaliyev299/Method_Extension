8bd00d2f323cab38474afb6acb5e3474993506eb
farruxaliyev299 <7hp245d@code.edu.az>
2022-04-05 03:10:58 +0400

    Added Task

---
+++ Extension/Extension/ExtensionMethods.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Extension
+{
+    internal static class ExtensionMethods
+    {
+
+        public static bool CustomContains(this string str,string contain)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            bool check=false;
+
+            int tempI = 0;
+            int tempJ = 0;
+
+            for (int i = 0; i < contain.Length; i++)
+            {
+                
+                for (int j = 0; j < str.Length; j++)
+                {
+                    Here:
+
+                    if (contain[i] == str[j])
+                    {
+                        sb.Append(str[j]);
+                        check = true;
+                    }
+
+                    if (check)
+                    {
+                        while (check)
+                        {
+                            tempI = i + 1;
+                            tempJ = j + 1;
+
+                            while(tempI<contain.Length && tempJ < str.Length)
+                            {
+                                if(contain[tempI] == str[tempJ])
+                                {
+                                    sb.Append(str[tempJ]);
+                                    tempI++;
+                                    tempJ++;
+                                }
+                                else
+                                {
+                                    sb = new StringBuilder("");
+                                    check = false;
+                                    j++;
+                                    goto Here;
+                                }
+                            }
+
+                            if(sb.ToString() == contain)
+                            {
+                                return true;
+                            }
+                            return false;
+                        }
+                        
+                    }
+
+                }
+            }
+
+            return false;
+        }
+
+        public static bool CustomContains(this string str,char ch)
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+                if(str[i] == ch)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool IsPrime(this int num)
+        {
+            int count = 0;
+            for (int i = 1; i <= num; i++)
+            {
+                if(num % i == 0)
+                {
+                    count++;
+                }
+            }
+
+            if (count > 2)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ Extension/Extension/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Extension
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int a = 17;
+
+            //Console.WriteLine(a.IsPrime());
+
+            string s = "Salam";
+
+            //char c = 'z';
+
+            //Console.WriteLine(s.CustomContains(c));
+
+            string con = "als";
+
+            Console.WriteLine(s.CustomContains(con));
+        }
+    }
+}
